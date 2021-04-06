c807bd102a55cbe3ce034cedcabc6ea4250c13e7
verak13 <verakovacevic98@gmail.com>
2021-04-06 18:18:09 +0200

    ispravke bagova

--- admin/src/main/resources/import.sql
+++ admin/src/main/resources/import.sql
@@ -2,3 +2,6 @@ INSERT INTO authority (name) VALUES ('ROLE_SUPERADMIN');
 
 INSERT INTO users (id, first_name, last_name, email, password) VALUES (1, 'Jane', 'Smith', 'superadmin@admin.com', '$2y$12$.PWVvztLIA1VBvOcpykig.CZPr78dwT3mVrtCda8YjjyNOas4P6j2');
 insert into user_authority (user_id, authority_id) values (1, 1);
+
+insert into requests (common_name, country_name, organization, organization_unit_name, state_name, locality_name, email, user_id) values ('cn', 'co', 'org', 'orgU', 'sn', 'ln', 'a@gmail.com', 1);
+insert into requests (common_name, country_name, organization, organization_unit_name, state_name, locality_name, email, user_id) values ('cn2', 'co', 'org', 'orgU', 'sn', 'ln', 'b@gmail.com', 2);
