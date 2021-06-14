2cae63a4209716598133a9bc619b4df9e4bc2028
specnazm <laketic.milena98@gmail.com>
2021-06-14 22:33:02 +0200

    fixed login error

--- hospital/src/main/resources/import.sql
+++ hospital/src/main/resources/import.sql
@@ -4,7 +4,7 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 INSERT INTO authority (name) VALUES ('ROLE_ADMIN');
 INSERT INTO authority (name) VALUES ('ROLE_USER');
 
-INSERT INTO users (id, first_name, last_name, email, password, last_login) VALUES (1, 'John', 'Smith', 'admin@gmail.com', '$2y$12$.PWVvztLIA1VBvOcpykig.CZPr78dwT3mVrtCda8YjjyNOas4P6j2', '2020-04-11');
+INSERT INTO users (first_name, last_name, email, password, last_login) VALUES ( 'John', 'Smith', 'admin@gmail.com', '$2y$12$.PWVvztLIA1VBvOcpykig.CZPr78dwT3mVrtCda8YjjyNOas4P6j2', '2020-04-11');
 insert into user_authority (user_id, authority_id) values (1, 1);
 
 
